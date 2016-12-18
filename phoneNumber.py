24aaf5ba0c285a8e6a624b0741aefd54322470dc
njerucyrus <njerucyrus5@gmail.com>
2016-12-18 16:33:30 +0300

    added more validations

--- phoneNumber.py
+++ phoneNumber.py
@@ -1,3 +1,4 @@
+
 class CleanPhoneNumber():
     def __init__(self, phone_number):
         self.phone_number = phone_number
@@ -6,14 +7,34 @@ class CleanPhoneNumber():
         phone_number = self.phone_number
         phone = list(phone_number)
         prefix = phone[0]
-        if prefix == '0' and len(phone_number)== 10 :
+        length = len(phone_number)
+    
+        if prefix == '0' and length== 10 and phone[1]== '7':
             phone[0] = '+254'
             return "".join(phone)
 
-        elif prefix == '+' and len(phone_number) == 13 :
+        elif prefix == '+' and length == 13  and phone[4] == '7':
             return str(phone_number)
 
-        elif len(phone_number)<10:
-            print 'invalid phone_number length'
+        elif length < 10 :
+            print 'invalid phone number length'
+
+        else:
+            print 'invalid phone number'
+
+if __name__ == '__main__':
+    phone_number = raw_input('Enter phone number:\n')
+    obj = CleanPhoneNumber(phone_number).validate_phone_number()
+    print obj
+        
+    
+            
+    
+        
+
+
+
+            
+            
         
         
